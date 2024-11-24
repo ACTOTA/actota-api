fd5ef8125d8f1ae7f8d9caa0ab3abc57d5552d3b
Cooksey99 <andrewcooksey42@gmail.com>
2024-11-23 19:58:45 -0600

    successfully returning all activities on /activities/get

--- src/models/activity.rs
+++ src/models/activity.rs
@@ -1,5 +1,4 @@
-use chrono::{DateTime, Utc};
-use mongodb::bson::oid::ObjectId;
+use mongodb::bson::{oid::ObjectId, DateTime};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -9,6 +8,6 @@ pub struct Activity {
     pub label: String,
     pub description: Option<String>,
     pub tags: Option<Vec<String>>,
-    pub created_at: Option<DateTime<Utc>>,
-    pub updated_at: Option<DateTime<Utc>>,
+    pub created_at: Option<DateTime>,
+    pub updated_at: Option<DateTime>,
 }
