16ff6faa7c8b162110efab392112cd7767dd49d3
Cooksey99 <andrewcooksey42@gmail.com>
2024-12-05 17:15:31 -0600

    added find by id

--- src/routes/featured_vacation.rs
+++ src/routes/featured_vacation.rs
@@ -1,15 +1,10 @@
-use crate::models::itinerary::FeaturedVacation;
+use crate::{models::itinerary::FeaturedVacation, services::itinerary_service::get_images};
 use actix_web::{web, HttpResponse, Responder};
 use bson::doc;
 use chrono::Utc;
-use cloud_storage::Client as StorageClient;
-use cloud_storage::ListRequest;
-use futures::future::join_all;
-use futures::StreamExt;
 use futures::TryStreamExt;
 use mongodb::Client;
-use std::{env, sync::Arc};
-use tokio::pin;
+use std::sync::Arc;
 
 /*
     /api/itineraries/featured/
@@ -19,63 +14,10 @@ pub async fn get_all(data: web::Data<Arc<Client>>) -> impl Responder {
     let collection: mongodb::Collection<FeaturedVacation> =
         client.database("Itineraries").collection("Featured");
 
-    let storage_client = StorageClient::default();
-
     match collection.find(doc! {}).await {
         Ok(cursor) => match cursor.try_collect::<Vec<FeaturedVacation>>().await {
-            Ok(mut activities) => {
-                // I have absolutely no idea how I got this to work
-                // This is fetching the images from the Google Cloud Storage bucket
-                let base_url = env::var("CLOUD_STORAGE_URL").unwrap_or("".to_string());
-                let bucket_name = env::var("ITINERARY_BUCKET").unwrap_or("".to_string());
-
-                // Create futures for each activity
-                let futures: Vec<_> = activities
-                    .iter_mut()
-                    .map(|activity| async {
-                        let list_request = ListRequest {
-                            prefix: Some(
-                                activity
-                                    .id
-                                    .unwrap_or(bson::oid::ObjectId::new())
-                                    .to_string(),
-                            ),
-                            ..Default::default()
-                        };
-
-                        let mut files = Vec::new();
-
-                        let stream = storage_client
-                            .object()
-                            .list(&bucket_name, list_request)
-                            .await?;
-                        pin!(stream);
-
-                        while let Some(object_result) = stream.next().await {
-                            if let Ok(object) = object_result {
-                                for item in object.items {
-                                    if item.name.ends_with(".jpg") || item.name.ends_with(".png") {
-                                        let url =
-                                            format!("{}/{}/{}", base_url, bucket_name, item.name);
-                                        files.push(url);
-                                    }
-                                }
-                            }
-                        }
-
-                        activity.images = Some(files);
-                        Ok(activity.clone())
-                    })
-                    .collect();
-
-                // Execute all futures concurrently
-                let processed_activities = join_all(futures)
-                    .await
-                    .into_iter()
-                    .filter_map(|r: Result<FeaturedVacation, cloud_storage::Error>| r.ok())
-                    .collect::<Vec<FeaturedVacation>>();
-
-                println!("\n\nProcessed activities: {:?}\n\n", processed_activities);
+            Ok(activities) => {
+                let processed_activities = get_images(activities).await;
                 HttpResponse::Ok().json(processed_activities)
             }
             Err(err) => {
---
+++ src/services/itinerary_service.rs
@@ -0,0 +1,61 @@
+use crate::models::itinerary::FeaturedVacation;
+use cloud_storage::Client as StorageClient;
+use cloud_storage::ListRequest;
+use futures::future::join_all;
+use futures::StreamExt;
+use std::env;
+use tokio::pin;
+
+pub async fn get_images(mut activities: Vec<FeaturedVacation>) -> Vec<FeaturedVacation> {
+    // I have absolutely no idea how I got this to work
+    // This is fetching the images from the Google Cloud Storage bucket
+    let base_url = env::var("CLOUD_STORAGE_URL").unwrap_or("".to_string());
+    let bucket_name = env::var("ITINERARY_BUCKET").unwrap_or("".to_string());
+
+    let storage_client = StorageClient::default();
+
+    // Create futures for each activity
+    let futures: Vec<_> = activities
+        .iter_mut()
+        .map(|activity| async {
+            let list_request = ListRequest {
+                prefix: Some(
+                    activity
+                        .id
+                        .unwrap_or(bson::oid::ObjectId::new())
+                        .to_string(),
+                ),
+                ..Default::default()
+            };
+
+            let mut files = Vec::new();
+
+            let stream = storage_client
+                .object()
+                .list(&bucket_name, list_request)
+                .await?;
+            pin!(stream);
+
+            while let Some(object_result) = stream.next().await {
+                if let Ok(object) = object_result {
+                    for item in object.items {
+                        if item.name.ends_with(".jpg") || item.name.ends_with(".png") {
+                            let url = format!("{}/{}/{}", base_url, bucket_name, item.name);
+                            files.push(url);
+                        }
+                    }
+                }
+            }
+
+            activity.images = Some(files);
+            Ok(activity.clone())
+        })
+        .collect();
+
+    // Execute all futures concurrently
+    return join_all(futures)
+        .await
+        .into_iter()
+        .filter_map(|r: Result<FeaturedVacation, cloud_storage::Error>| r.ok())
+        .collect::<Vec<FeaturedVacation>>();
+}
