c6b14b45cc99cc30e9e3deda4182249362ba0547
Cooksey99 <andrewcooksey42@gmail.com>
2025-02-25 21:09:08 -0600

    need to update callback

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,5 +1,6 @@
 pub mod account;
 pub mod activity;
+pub mod google_auth;
 pub mod interests;
 pub mod itinerary;
 pub mod location;
--- src/routes/account/mod.rs
+++ src/routes/account/mod.rs
@@ -1,4 +1,5 @@
 pub mod account_info;
 pub mod auth;
 pub mod favorites;
+pub mod google_auth;
 pub mod payment_methods;
